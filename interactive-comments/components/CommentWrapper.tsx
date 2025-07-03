45e4848d61e965279ef16fb6520bd90cf35a7551
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2025-07-03 14:37:47 +0300

    Style the main wrapper

--- interactive-comments/components/CommentWrapper.tsx
+++ interactive-comments/components/CommentWrapper.tsx
@@ -3,10 +3,10 @@ import Comment from './Comment'
 
 const CommentWrapper = () => {
   return (
-    <div className='w-full flex flex-col justify-center items-center gap-4'>
+    <div className='w-full flex flex-col justify-center items-end gap-4'>
       <Comment/>
 
-      <div className='w-full flex flex-col justify-center items-center gap-4 border-l-[1px] border-l-slate-400 pl-5'>
+      <div className='w-[90%] flex flex-col justify-center items-center gap-4 border-l-[1px] border-l-slate-400 pl-5'>
         <Comment/>
         <Comment/>
         <Comment/>
